79c407b4fb904ad6ba80cb5f36709d6b17a29ba1
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-09-11 09:27:45 +0000

    automatically backed up by learn

--- src/components/PoemWriter.js
+++ src/components/PoemWriter.js
@@ -1,22 +1,39 @@
 import React from "react";
+import PropTypes from "prop-types";
+//read more about proptypes here: https://reactjs.org/docs/typechecking-with-proptypes.html
 
-class PoemWriter extends React.Component {
+class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = { message: "" };
   }
+
+  setMessage = event => {
+    this.setState({ message: event.target.value });
+  };
 
   render() {
     return (
       <div>
-        <textarea rows="3" cols="60" />
-        <div id="poem-validation-error" style={{ color: "red" }}>
-          This poem is not written in the right structure!
-        </div>
+        <strong>Your message:</strong>
+        <input
+          type="text"
+          value={this.state.message}
+          onChange={this.setMessage}
+        />
+        <span>{this.props.maxChars - this.state.message.length}</span>
       </div>
     );
   }
 }
 
-export default PoemWriter;
+TwitterMessage.propTypes = {
+  maxChars: PropTypes.number,
+};
+
+TwitterMessage.defaultProps = {
+  maxChars: 140,
+};
+
+export default TwitterMessage;
